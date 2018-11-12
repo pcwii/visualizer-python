78be0ee3987aae6365cc0f71e80e36f103690e0c
philc <philc@arcx.com>
2018-11-12 11:27:21 -0500

    ready to test on linux

--- pygame_VU.py
+++ pygame_VU.py
@@ -12,9 +12,13 @@ PeakR = 0
 
 # fadecandy variables
 numLEDs = 64
+maxLEDs = numLEDs / 2
+maxBars = 40
 client = opc.Client('localhost:7890')
 pixels = [(0, 0, 0)] * numLEDs
 brightness = 255
+y_pos = 20
+r_pos = 30
 
 def led_on(led_index, Red, Green, Blue):
     # all_lights_off()
@@ -55,10 +59,10 @@ while True:  # main application loop
     data = stream.read(1024)
     ldata = audioop.tomono(data, 2, 1, 0)
     amplitudel = ((audioop.max(ldata, 2)) / 32767)
-    LevelL = (int(41 + (20 * (math.log10(amplitudel + (1e-40))))))
+    LevelL = (int(maxBars + (20 * (math.log10(amplitudel + (1e-40)))))) # changed 41 to 32
     rdata = audioop.tomono(data, 2, 0, 1)
     amplituder = ((audioop.max(rdata, 2)) / 32767)
-    LevelR = (int(41 + (20 * (math.log10(amplituder + (1e-40))))))
+    LevelR = (int(maxBars + (20 * (math.log10(amplituder + (1e-40))))))
 
     # Use the levels to set the peaks
     if LevelL > PeakL:
@@ -66,7 +70,7 @@ while True:  # main application loop
         brightness = 255
     elif PeakL > 0:
         PeakL = PeakL - 0.2
-        brightness = brightness - (brightness * 0.025)
+        brightness = brightness - (brightness * 0.023)
     if LevelR > PeakR:
         PeakR = LevelR
     elif PeakR > 0:
@@ -87,29 +91,43 @@ while True:  # main application loop
     leds_off()
     # Draw the boxes
     for i in range(0, LevelL):
-        if i < 20:
+        if i < y_pos:
             pygame.draw.rect(DISPLAYSURF, (0, 192, 0), (10, (475 - i * 12), 30, 10))
-            led_on(i, 0, 192, 0)
-        elif i >= 20 and i < 30:
+        elif i >= y_pos and i < r_pos:
             pygame.draw.rect(DISPLAYSURF, (255, 255, 0), (10, (475 - i * 12), 30, 10))
-            led_on(i, 255, 255, 0)
         else:
             pygame.draw.rect(DISPLAYSURF, (255, 0, 0), (10, (475 - i * 12), 30, 10))
-            led_on(i, 255, 0, 0)
-    neg_start = numLEDs - 1
     for i in range(0, LevelR):
-        if i < 20:
+        if i < y_pos:
             pygame.draw.rect(DISPLAYSURF, (0, 192, 0), (90, (475 - i * 12), 30, 10))
-            led_on(neg_start - i, 0, 192, 0)
-        elif i >= 20 and i < 30:
+        elif i >= y_pos and i < r_pos:
             pygame.draw.rect(DISPLAYSURF, (255, 255, 0), (90, (475 - i * 12), 30, 10))
-            led_on(neg_start - i, 255, 255, 0)
         else:
             pygame.draw.rect(DISPLAYSURF, (255, 0, 0), (90, (475 - i * 12), 30, 10))
+    # Draw the LEDs
+    LedMultiplier = (maxLEDs / maxBars)
+    LedLevelL = int(LedMultiplier * LevelL)
+    LedLevelR = int(LedMultiplier * LevelR)
+    LedY_pos = int(y_pos * LedMultiplier)
+    LedR_pos = int(r_pos * LedMultiplier)
+    for i in range(0, LedLevelL):
+        if i < LedY_pos:
+            led_on(i, 0, 127, 0)
+        elif i >= LedY_pos and i < LedR_pos:
+            led_on(i, 255, 255, 0)
+        else:
+            led_on(i, 255, 0, 0)
+    neg_start = numLEDs - 1
+    for i in range(0, LedLevelR):
+        if i < LedY_pos:
+            led_on(neg_start - i, 0, 192, 0)
+        elif i >= LedY_pos and i < LedR_pos:
+            led_on(neg_start - i, 255, 255, 0)
+        else:
             led_on(neg_start - i, 255, 0, 0)
     # Draw the peak bars
     pygame.draw.rect(DISPLAYSURF, (255, 255, 255), (10, (485 - int(PeakL) * 12), 30, 2))
-    led_on(int(PeakL), brightness, brightness, brightness)
+    led_on(int(PeakL * LedMultiplier), brightness, brightness, brightness)
     pygame.draw.rect(DISPLAYSURF, (255, 255, 255), (90, (485 - int(PeakR) * 12), 30, 2))
-    led_on(neg_start - int(PeakR), brightness, brightness, brightness)
+    led_on(neg_start - int(PeakR * LedMultiplier), brightness, brightness, brightness)
     pygame.display.update()
